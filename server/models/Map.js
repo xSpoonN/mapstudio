cf4789cb70a2844fc6807ecc8f0b874ffdbf4333
WeiQi-0414 <weiqi0414@gmail.com>
2023-11-20 00:23:01 -0500

    try map schema

---
+++ server/models/Map.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const mapSchema = new mongoose.Schema({
+    author: { type: ObjectId, ref: 'User', required: true },
+    isPublished: { type: Boolean, required: true },
+    title: { type: String, required: true },
+    description: { type: String, default: '' },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
+    comments: [{ type: ObjectId, ref: 'Comment' }],
+    mapFile: { type: Buffer, required: true },
+    creationDate: { type: Date, required: true, default: Date.now },
+    updateDate: { type: Date, required: true, default: Date.now }
+});
+
+module.exports = mongoose.model('Map', mapSchema);
