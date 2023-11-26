ed4d995b6801b3cc978cc5f815ceb7ac0e95131f
Kevin <kt4cty@gmail.com>
2023-11-25 22:47:39 -0500

    revise tests and resolve port conflict issue

--- server/tests/comments.test.js
+++ server/tests/comments.test.js
@@ -4,18 +4,18 @@ const app = require('../index');
 describe('/comments', () => {
     it('should get a comment', async () => {
         const res = await request(app).post('/comment/allcomments').send({
-            id: ['655fcf96015e00ad5a534012']
+            ids: ['655fcf96015e00ad5a534012']
         })
         
         expect(res.statusCode).toEqual(200);
-        expect(res.body.comments).toHaveProperty('author', 'preston');
-        expect(res.body.comments).toHaveProperty('content', 'nope and the Alps dont exist');
-        expect('likes' in res.body.comments);
-        expect('dislikes' in res.body.comments);
-        expect('authorId' in res.body.comments);
-        expect('likeUsers' in res.body.comments);
-        expect('dislikeUsers' in res.body.comments);
-        expect('publishedDate' in res.body.comments);
+        expect(res.body.comments[0]).toHaveProperty('author', 'preston');
+        expect(res.body.comments[0]).toHaveProperty('content', 'nope and the Alps dont exist');
+        expect('likes' in res.body.comments[0]);
+        expect('dislikes' in res.body.comments[0]);
+        expect('authorId' in res.body.comments[0]);
+        expect('likeUsers' in res.body.comments[0]);
+        expect('dislikeUsers' in res.body.comments[0]);
+        expect('publishedDate' in res.body.comments[0]);
     });
 });
 
